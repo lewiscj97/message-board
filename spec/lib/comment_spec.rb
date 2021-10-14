4c0196f3f445d9aed10c42bdee8e6d7f7b9cf5d1
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-14 10:48:01 +0100

    refactor(*): truncate comments relation before each test

--- spec/lib/comment_spec.rb
+++ spec/lib/comment_spec.rb
@@ -5,7 +5,12 @@ describe Comment do
   describe '#create' do
     it 'lets the user create a new comment on a post' do
       post = Post.create('Lewis Jones', 'This is my first message!')
-      post_id = post.id
+
+      connection = PG.connect(dbname: 'message_board_test')
+      data = connection.exec("SELECT * FROM posts;")
+
+      post_id = data.first['id']
+      
       comment = Comment.create('Foo', 'This is a comment', post_id)
       
       expect(comment.name).to eq 'Foo'
